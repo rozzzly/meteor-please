ed557ba586808a487a17ed47cfcd572bcb417fe6
rozzzly <pclienau@gmail.com>
2015-07-08 04:36:01 -0400

    hopefully fixed something for once

--- scripts/rh-setup-nginx.sh
+++ scripts/rh-setup-nginx.sh
@@ -17,13 +17,17 @@ sudo mkdir -p /etc/nginx
 sudo yum install -y nginx
 sudo chown root:developers /etc/nginx/nginx.conf
 sudo chmod g+w /etc/nginx.conf
+#
 sudo mkdir /etc/nginx/sites-enabled
+sudo chown root:developers /etc/nginx/sites-enabled
+sudo chmod g+w /etc/sites-enabled
 # nginx daemon
 sudo systemctl enable nginx
 sudo systemctl start nginx
+#
 # firewalld --- turn this off because its ruining everything
-#sudo systemctl enable firewalld
-#sudo systemctl start firewalld
-#sudo firewall-cmd --permanent --zone=public --add-service=http
-#sudo firewall-cmd --permanent --zone=public --add-service=https
-#sudo firewall-cmd --reload
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
