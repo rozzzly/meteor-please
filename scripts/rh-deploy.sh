23c5ed19eff6b9b3069cd2b0f2ba4fd333f23105
rozzzly <pclienau@gmail.com>
2015-07-09 03:09:17 -0500

    fix deploy service linking bug

--- scripts/rh-deploy.sh
+++ scripts/rh-deploy.sh
@@ -3,7 +3,6 @@
 sudo systemctl stop <%= appName %>.service
 sudo chown -R <%= appUser %> <%= appRemoteTargetPath %>
 #su <%= appUser %>
-whoami
 cd <%= appRemoteTargetPath %>
 
 # unpack bundle / overwrite previous
@@ -14,6 +13,9 @@ rm -rf <%= appName %>.tar.gz
 cd bundle/programs/server/
 npm install
 exit
+
+# symlink service config from meteor-please to the base directory so it can get picked up
+sudo ln -s /etc/systemd/system/meteor-please/<%= appName %>.service /etc/systemd/system/<%= appName %>.service
 
 # restart daemon
 sudo systemctl daemon-reload
